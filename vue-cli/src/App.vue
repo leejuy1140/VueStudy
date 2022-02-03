e8ed062826f868d72ff615e9fd2d033a35e5eee9
leejuy1140 <leejuy1140@gmail.com>
2022-02-04 01:36:05 +0900

    Vue CLI

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!--
+    HelloWorld라는 컴포넌트 등록 
+    <hello-world></hello-world>와 동일 
+  -->
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  //인스턴스 옵션 속성 또는 컴포넌트 옵션 속성
+  name: 'App',
+  components: {
+    // 'hello-world': HelloWorld의 축약
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,26 @@
+var appHeader = {
+    template: '<div>Header</div>',
+    methods: {
+        addNum: function() {
+        }
+    }
+}
+
+<template>
+  <!-- HTML -->
+  <div>Header</div>
+</template>
+
+<script>
+export default {
+    //Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function() {
+        }
+    }
+}
+</script>
+
+<style>
+    /* css */
+</style>
