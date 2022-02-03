cd581355160003e951d1e6a07faf3dd269842076
leejuy1140 <leejuy1140@gmail.com>
2022-02-04 02:40:27 +0900

    Vue2 CLI and basic props & emit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- App이라는 컴포넌트에서 AppHeader컴포넌트로 propsdata로 str을 보냄 -->
+    <app-header 
+      v-bind:propsdata="str" 
+      v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  data : function() {
+    return {
+      //이걸 AppHeader 컴포넌트로 Props 할 것!
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = this.str + '1';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <h1>{{ propsdata }}</h1>
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+  methods: {
+		//버튼을 누를 때마다 propsdata의 내용 변경하기!
+    sendEvent: function() {
+      this.$emit('renew')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
