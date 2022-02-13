385980e86472c9b5b36168df6839766c6a7fc0b8
leejuy1140 <leejuy1140@gmail.com>
2022-02-13 18:31:18 +0900

    create component - TodoHeader, TodoInput

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+      <h1>TODO it!</h1>
+  </header>
+</template>
+
+// scoped: Vue 싱글파일 컴포넌트에서 지원하는 속성으로 해당 컴포넌트 안에서만 유효한 스타일 속성
+// 원래 상위 html요소의 스타일을 아래로 상속받는데 그게 무시가 됨
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    /* rem은 font의 크기에 따라 배율이 정해짐 */
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: "" //새롭게 입력되는 text
+        }
+    },
+    methods: {
+        addTodo: function() {
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = "";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
