d1979d01aa5efed4052b73c33734cb3e1a5669d4
leejuy1140 <leejuy1140@gmail.com>
2022-02-27 23:56:15 +0900

    Apply Vuex state

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -5,6 +5,9 @@ import Vuex from 'vuex'
 //Vue를 사용하는 모든 영역에 특정 기능을 전역으로 추가하고 싶을 때
 Vue.use(Vuex);
 
-//store는 밖에서 사용할 수 있게 설정
+//store는 밖에서 사용할 수 있게 export 설정
 export const store = new Vuex.Store({
+    state: {
+        headerText: 'TODO it!'
+    }
 });
