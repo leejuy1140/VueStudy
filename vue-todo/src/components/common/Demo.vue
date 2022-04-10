9288b92fc18077053a0ba02f6383e626f4f043d3
leejuy1140 <leejuy1140@gmail.com>
2022-04-10 22:37:47 +0900

    Advantage of helper Vuex

---
+++ vue-todo/src/components/common/Demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="root">
+      <p>{{ originalPrice }}</p>
+      <p>{{ doublePrice }}</p>
+      <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+//Helper 함수가 주는 간편함
+import { mapGetters } from 'vuex';
+
+export default {
+    computed: {
+        ...mapGetters(['originalPrice', 'doublePrice', 'triplePrice']),
+        // originalPrice() {
+        //     this.$store.getters.originalPrice
+        // },
+        // doublePrice() {
+        //     this.$store.getters.doublePrice
+        // },
+        // triplePrice() {
+        //     this.$store.getters.triplePrice
+        // }
+    }
+}
+</script>
+
+<style>
+
+</style>
